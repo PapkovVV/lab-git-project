7253629aaf65ce89fe8193f6b25bac25692f6a77
ValenPapkov <valentinpapkov77@gmail.com>
2025-01-26 20:41:40 +0300

    Initial project setup

---
+++ GitPZ/GitPZ/tests/Test_Main.cs
@@ -0,0 +1,12 @@
+﻿using GitPZ.src;
+
+namespace GitPZ.tests
+{
+    internal class Test_Main
+    {
+        public static void TestPrint()
+        {
+            Utils.PrintMessage("Test");
+        }
+    }
+}
